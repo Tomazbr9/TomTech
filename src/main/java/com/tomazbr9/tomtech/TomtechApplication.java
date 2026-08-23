0461a33d209cb52d52d0edbaf2bdb131d12f5f23
tomaz <tomazcraft@gmail.com>
2026-08-23 19:53:58 -0300

    first commit

---
+++ src/main/java/com/tomazbr9/tomtech/TomtechApplication.java
@@ -0,0 +1,13 @@
+package com.tomazbr9.tomtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomtechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomtechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tomazbr9/tomtech/TomtechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomazbr9.tomtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomtechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
